c81b1aa7b6962f7099fb6f880928a3c1d7afe849
Jeffrey Cate <Jeff@MacBook.local>
2019-10-03 09:17:27 -0500

    Activity 3 Complete

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+// Get the connection string
+const db = config.get('mongoURL');
+
+//Connect to MongoDB
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(db, {
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        //console.log('error here');
+        console.error(error.message);
+
+        //Exit with falure code
+        process.exit(1);
+    }
+};
+
+export default connectDatabase;

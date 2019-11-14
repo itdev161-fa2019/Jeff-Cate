9e62bbec57fa68c0a33bcf3ab21d0f12bd9c0c0a
Jeffrey Cate <Jeff@MacBook.local>
2019-11-13 19:40:49 -0600

    Create endpoint to get post

--- server.js
+++ server.js
@@ -204,3 +204,18 @@ app.post(
         }
     }
 );
+
+/**
+ * @route
+ * @desc Get posts
+ */
+app.get('/api/posts', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
